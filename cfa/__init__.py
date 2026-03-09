d1f8728b2ac9e2f5ea2caabe85a607bbca5342c1
Yunis <yuanhongwu60@gmail.com>
2026-03-09 13:55:14 -0400

    update files

--- cfa/__init__.py
+++ cfa/__init__.py
@@ -1,5 +1,4 @@
 from .combinatorial_fusion import cfa_single_layer, performance_plot, RSC_graph
-from .fusion_function import compute_cd_ds, compute_performance
+from .fusion_function import compute_cd_ds, compute_performance, average_score_combination, average_rank_combination, weighted_score_combination_by_ds,weighted_rank_combination_by_ds
 
-__all__ = ["cfa_single_layer","performance_plot", "RSC_graph", "compute_cd_ds", "compute_performance"]
-
+__all__ = ["cfa_single_layer","performance_plot", "RSC_graph", "compute_cd_ds", "compute_performance", "average_score_combination", "average_rank_combination", "weighted_score_combination_by_ds", "weighted_rank_combination_by_ds"]
